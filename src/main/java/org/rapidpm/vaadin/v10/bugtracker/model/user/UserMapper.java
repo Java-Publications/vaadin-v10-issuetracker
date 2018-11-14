529b190cb81a1a59a4528f270e825b93ffd57e6f
dve <dve@vergien.net>
2018-11-14 11:06:08 +0100

    clean up mappers

--- src/main/java/org/rapidpm/vaadin/v10/bugtracker/model/user/UserMapper.java
+++ src/main/java/org/rapidpm/vaadin/v10/bugtracker/model/user/UserMapper.java
@@ -1,29 +1,12 @@
 package org.rapidpm.vaadin.v10.bugtracker.model.user;
 
-import java.util.Set;
-
 import org.mapstruct.Mapper;
-import org.rapidpm.vaadin.v10.bugtracker.model.userrole.UserRole;
+import org.mapstruct.Mapping;
 import org.rapidpm.vaadin.v10.bugtracker.model.userrole.UserRoleMapper;
 import org.rapidpm.vaadin.v10.bugtracker.persistence.entities.UserEntity;
-import org.rapidpm.vaadin.v10.bugtracker.persistence.entities.UserRoleEntity;
 
-@Mapper(
-    uses = {UserRoleMapper.class},
-    componentModel = "cdi")
+@Mapper(uses = {UserRoleMapper.class}, componentModel = "cdi")
 public interface UserMapper {
-
-  //UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
-
-  //@Mapping(source = "email", target = "seatCount")
-  User toUser(UserEntity e);
-
-  default UserRole toUserRole(UserRoleEntity e) {
-    return UserRole.byNameProperty(e.getName());
-  }
-
-  //Mapping Collection element
-  Set<UserRole> toRoles(Set<UserRoleEntity> userRoleEntities);
-
-
+  @Mapping(target = "userId", source = "id")
+  User toUser(UserEntity userEntity);
 }
